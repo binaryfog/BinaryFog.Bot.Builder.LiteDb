604e790303dfc194b70b2ec975fe41edbabce386
Catalin Hatmanu <harapu@hotmail.com>
2019-06-25 09:19:57 -0400

    Changed Memory storage

--- StateManagementBot/Startup.cs
+++ StateManagementBot/Startup.cs
@@ -25,7 +25,7 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
             // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.) 
-            services.AddSingleton<IStorage, MemoryStorage>();
+            services.AddSingleton<IStorage, BinaryFog.Bot.Builder.LiteDb.LiteDbStorage>();
 
             // Create the User state. 
             services.AddSingleton<UserState>();
